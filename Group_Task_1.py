43f77e5bd01699e47af62584f1f72ba3c6f22ed3
PeizhiYang <Peizhi@Peizhis-MacBook-Air.local>
2015-02-11 11:06:10 -0500

    Group Task 1

---
+++ Group_Task_1.py
@@ -0,0 +1,32 @@
+from __future__ import division
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+import os
+##_______________________________________________
+## import files, stack and merging
+main_dir = "/Users/Peizhi/Desktop/Duke 2015 SPRING/PUBPOL 590/"
+root = main_dir + "raw/"
+paths = [ root + "File" + str(v) + ".txt" for v in range(1,7) ]
+missing = ['.', 'NA', 'NULL', '','999999999','9999999']
+list_of_dfs = [ pd.read_table(v,sep = " ",names = ['panid', 'date', 'kwh'],na_values = missing) for v in paths]
+len(list_of_dfs)
+type(list_of_dfs)
+type(list_of_dfs[0])
+
+df = pd.concat(list_of_dfs, ignore_index = True)
+missing = ['.', 'NA', 'NULL', '','999999999','9999999']
+## problematic df_assign = pd.read_csv(root + "SME and Residential allocations.csv",usecols = [0,5])
+df_assign = pd.read_csv(root + "SME and Residential allocations.csv",usecols = ['ID','Code','Residential - Tariff allocation','Residential - stimulus allocation','SME allocation'],na_values = missing)
+df1 = df_assign.rename(columns = {'ID':'panid'})
+df2 = pd.merge(df, df1, on = 'panid' )
+
+## cleaning the missing/duplicates
+
+df2.duplicates(['panid','date'])
+df2.drop_duplicates(['panid','date'])
+
+
+## prolem1: DST missing/ extra entries
+## problem2: floor size "999999999"
+
