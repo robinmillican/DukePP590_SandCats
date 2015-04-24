b013a062b9666eca651c7457a1bb2c3e4011ecc1
Danton Noriega <ultinomics@github.com>
2015-04-24 16:18:42 -0400

    graded and comments for task 4.

--- task4/task4codefinal.py
+++ task4/task4codefinal.py
@@ -1,3 +1,5 @@
+"""9/9 pts"""
+
 from __future__ import division
 from pandas import Series, DataFrame
 import pandas as pd
@@ -5,9 +7,9 @@ import numpy as np
 import statsmodels.api as sm
 import os
 
- 
-##============Section 0 
-main_dir =  "/Users/Peizhi/Desktop/Duke 2015 SPRING/PUBPOL 590/data/task4/"
+
+##============Section 0
+main_dir =  '/Users/dnoriega/Dropbox/pubpol590_sp15/data_sets/CER/tasks/4_task_data/'
 
 os. chdir(main_dir)
 from logit_functions import *
@@ -63,7 +65,7 @@ df_pretrial = df.drop(drop, axis=1)
 for i in tariffs:
     for j in stimuli:
         logit_results, df_logit = do_logit(df_pretrial, i, j, add_D=None, mc=False)
-  
+
 grp = df_logit.groupby('tariff')
 df_mean = grp.mean().transpose()
 
@@ -76,9 +78,9 @@ top = df_mean['C'] - df_mean['E']
 bottom = np.sqrt(df_s['C']**2/df_n['C'] + df_s['E']**2/df_n['E'])
 tstats = top/bottom
 sig = tstats[np.abs(tstats) > 2]
-sig.name = 't-stats'      
+sig.name = 't-stats'
 
-        
+
 ##===============Section 2
 
 df_logit['p_hat'] = logit_results.predict()
@@ -87,8 +89,8 @@ df_logit['trt'] = 0 + (df_logit['tariff'] == 'C')
 df_logit['w'] = np.sqrt((df_logit['trt']/df_logit['p_hat'] + (1-df_logit['trt'])/ (1- df_logit['p_hat']) ))
 df_w = df_logit[['ID','trt','w']]
 ##================ section 3
- 
-main_dir =  "/Users/Peizhi/Desktop/Duke 2015 SPRING/PUBPOL 590/data/task4/"
+
+main_dir =  '/Users/dnoriega/Dropbox/pubpol590_sp15/data_sets/CER/tasks/4_task_data/'
 df = pd.read_csv(main_dir + 'task_4_kwh_long.csv')
 
 os. chdir(main_dir)
@@ -97,7 +99,7 @@ from fe_functions import *
 df = pd.merge(df_w, df)
 df['TP'] = df['trt'].apply(int) * df.trial.apply(int)
 
-df['log_kwh'] = (df['kwh']+1).apply(log)
+df['log_kwh'] = (df['kwh']+1).apply(np.log)
 df['mo_str'] = np.array(["0" + str(v) if v < 10 else str(v) for v in df['month']])
 df['ym'] = df['year'].apply(str) + "_" + df['mo_str']
 
@@ -112,21 +114,21 @@ y = demean(y, ids)
 X = demean(X, ids)
 
 ##model without weights
-fe_model = sm.OLS(y, X) 
-fe_results = fe_model.fit() 
+fe_model = sm.OLS(y, X)
+fe_results = fe_model.fit()
 print(fe_results.summary())
 
 
 ##model with weights
-y = y*w 
-nms = X.columns.values 
-X = np.array([x*w for k, x in X.iteritems()]) 
-X = X.T 
-X = DataFrame(X, columns = nms) 
+y = y*w
+nms = X.columns.values
+X = np.array([x*w for k, x in X.iteritems()])
+X = X.T
+X = DataFrame(X, columns = nms)
 
-fe_w_model = sm.OLS(y, X) 
-fe_w_results = fe_w_model.fit() 
-print(fe_w_results.summary()) 
+fe_w_model = sm.OLS(y, X)
+fe_w_results = fe_w_model.fit()
+print(fe_w_results.summary())
 
 
 
